3c1af52e44b590b8bddb42fee1b2719866d7faba
norby <norby@genx.dk>
2011-01-22 23:11:32 +0000

    Moved CircleMover into its own file

--- main.cpp
+++ main.cpp
@@ -128,26 +128,6 @@ public:
     }
 };
 
-class CircleMover : public IListener<Core::ProcessEventArg> {
-    TransformationNode* node;
-    float pos;
-    Vector<3,float> offset;
-    Vector<2,float> circle;
-    float speed;
-public:
-    CircleMover(TransformationNode *n, 
-                Vector<2,float> cir=(Vector<2,float>(10,100)),
-                float s=2)
-        : node(n),pos(0),offset(n->GetPosition()), circle(cir),speed(s) {}
-    void Handle(Core::ProcessEventArg arg) {
-        float delta = arg.approx/1000000.0;
-        pos += delta;
-        node->SetPosition(Vector<3,float>(offset[0]+circle[0]*sin(pos*speed),
-                                          offset[1],
-                                          offset[2]+circle[1]*cos(pos*speed)));
-    }
-};
-
 
 // Global stuff only used for setup.
 IEngine* engine;
