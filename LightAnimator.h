a9b059062fa66108b0438207fac88ad02f1fff04
cpvc <cpvc@openengine.dk>
2011-01-23 23:08:41 +0000

    now real disco lighting

--- LightAnimator.h
+++ LightAnimator.h
@@ -11,14 +11,24 @@ class LightAnimator : public OpenEngine::Core::IListener<OpenEngine::Core::Proce
  protected:
     OpenEngine::Geometry::Light* light;
     float t;
+    bool disco;
  public:
  LightAnimator(OpenEngine::Geometry::Light* light) : light(light) {
         t = 0.0f;
+        disco = true;
     }
     void Handle(OpenEngine::Core::ProcessEventArg arg) {
         t += (arg.approx * 1e-6);
-        float value = ( (sin( (t/40.0f) * 2*OpenEngine::Math::PI) + 1.0f)/2.0f) * 0.1;
-        light->ambient = Vector<4,float>(0.4,0.45+value,0.4,1.0);
+        if (!disco) {
+            float timeFactor = 40.0f;
+            float value = (sin( (t/timeFactor) * 2*OpenEngine::Math::PI) + 1.0f)/2.0f;
+            light->ambient = Vector<4,float>(0.4,0.45+(value*0.1),0.4,1.0);
+        } else {
+            float value1 = (sin( (t/3.0f) * 2*OpenEngine::Math::PI) + 1.0f)/2.0f;
+            float value2 = (sin( (t/5.0f) * 2*OpenEngine::Math::PI) + 1.0f)/2.0f;
+            float value3 = (sin( (t/7.0f) * 2*OpenEngine::Math::PI) + 1.0f)/2.0f;
+            light->ambient = Vector<4,float>(0.7+value1*0.3,0.5+value2*0.5,0.3+value3*0.7,1.0);
+        }
     }
 };
 
